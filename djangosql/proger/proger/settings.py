54401a84e76d8010c861e5f4a0340a8c42c1b584
Boomer9955 <rombich@bk.ru>
2021-12-07 16:14:18 +0300

    _

--- djangosql/proger/proger/settings.py
+++ djangosql/proger/proger/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c3#-d#y30bjb-@y2jk!(c8fq_j%_ek@u@!h$uzw-&83espk16_'
+SECRET_KEY = os.getenv('djangosql-env-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
